a5f67caf16af0b742cc9e3c5628a484c09976dc6
Marcel <marcelfonteles@gmail.com>
2019-05-08 02:18:02 +0000

    corrigindo e entendendo o ngrResource factory

--- app/assets/javascripts/app/controllers/EventsCtrl.js
+++ app/assets/javascripts/app/controllers/EventsCtrl.js
@@ -1,12 +1,17 @@
 var app = angular.module("angularExample");
 
-app.controller("EventsCtrl", ["$scope", "Event", function($scope, Event) {
+app.controller("EventsCtrl", ["$scope", "Event", "Evento", function($scope, Event, Evento) {
   $scope.events = Event.query();
+  // $scope.eventos = Evento.query({ id: 1});
+  
+  $scope.filterEvents = function() {
+    
+  }
   
   
    $scope.addEvent = function() {
     if (!valid()) { return false; }
-
+    // usa o method post, caralho!1!
     Event.save($scope.event,
       function(response, _headers) {
         $scope.events.push(response);
--- app/assets/javascripts/app/factories/event.js
+++ app/assets/javascripts/app/factories/event.js
@@ -2,4 +2,7 @@ var app = angular.module("angularExample");
 
 app.factory("Event", ["$resource", function($resource) {
   return $resource("/api/events/:id.json", { id: "@id"});
-}]);
+}])
+  .factory("Evento", ["$resource", function($resource) {
+    return $resource("/api/events/1/:id.json", {id: "@id"})
+  }])
